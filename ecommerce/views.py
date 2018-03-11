24eda817836fd10ee2342d2f3c21541c5435e675
Oussail <oussail.alaoui@gmail.com>
2018-03-11 21:17:59 +0000

    def login authentification

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,10 +1,35 @@
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from .forms import ContactForm
+from django.shortcuts import render, redirect
+from .forms import ContactForm, LoginForm
 
 
 def index(request):
 	return render(request, "index.html", {})
+
+
+def login_auth(request):
+	form = LoginForm(request.POST or None)
+	context = {
+		"form": form
+	}
+	if form.is_valid():
+		username = form.cleaned_data.get('username')
+		password = form.cleaned_data.get('password')
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			print(request.user.is_authenticated)
+			login(request, user)
+			print(request.user.is_authenticated)
+			# context['form'] = LoginForm()
+			return redirect("/")
+		else:
+			print('error')
+	return render(request, "auth/login.html", context)
+
+
+def register(request):
+	return render(request, "auth/register.html", {})
 
 
 def contact(request):
