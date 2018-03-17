99244b8b518cb74c6f2dd696fa3dc2050b994e00
Oussail <oussail.alaoui@gmail.com>
2018-03-17 10:33:38 +0000

    remove detailed functions

--- products/views.py
+++ products/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 
 from .models import Product
@@ -7,35 +6,9 @@ from .models import Product
 class ProductListView(ListView):
 	queryset = Product.objects.all()
 
-	def get_context_data(self, *args, **kwargs):
-		context = super(ProductListView, self).get_context_data(*args, **kwargs)
-		print(context)
-		return context
-
-
-def product_list_view(request):
-	queryset = Product.objects.all()
-	context = {
-		'object_list': queryset
-	}
-	return render(request, "products/product_list.html", context)
-
 
 class ProductDetailView(DetailView):
 	queryset = Product.objects.all()
-
-	def get_context_data(self, *args, **kwargs):
-		context = super(ProductDetailView, self).get_context_data()
-		print(context)
-		return context
-
-
-def product_detail_view(request, pk=None):
-	instance = get_object_or_404(Product, pk=pk)
-	context = {
-		'object': instance
-	}
-	return render(request, "products/product_detail.html", context)
 
 
 
